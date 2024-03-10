1fbaf1c4b1b814b04ef4e85ba0509645b0184210
Ilhomjon <mynameisilhom@gmail.com>
2024-03-11 01:59:45 +0500

    BRR

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-# TOKEN = '7164579995:AAHe3Odd5Mwpovfgk0A4GV8QxfE8KeScp_U'
-TOKEN = '6129113361:AAGfSoxHY8uhY_tXizDKHhTCpvAA2E6sfdU'
+TOKEN = '7164579995:AAHe3Odd5Mwpovfgk0A4GV8QxfE8KeScp_U'
+# TOKEN = '6129113361:AAGfSoxHY8uhY_tXizDKHhTCpvAA2E6sfdU'
 DATABASE = "mysql+mysqlconnector://root:root@localhost/ramazon"
 ADMINS = 505825849  # Замените на реальные user_id администраторов вашего бота
 
 
 host = "localhost"
 user = "root"
-# password = "ramazonserver"
-password = "root"
+password = "ramazonserver"
+# password = "root"
 database = "ramazon"
